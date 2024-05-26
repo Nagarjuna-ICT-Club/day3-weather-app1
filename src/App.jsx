c165ce5ae2566e95a89e244d820353df03ae9480
giver-kdk <giverbro13@gmail.com>
2024-05-27 01:26:53 +0545

    Image Clearn Update

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,6 @@ function App() {
   let [weatherData, setWeatherData] = useState(null);
   let [dataError, setDataError] = useState(false);
 
-  let randomSky = "https://source.unsplash.com/random/1920x1080/?sky";
-
   useEffect(() => {
     setLoading(true);
     let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=2a1f42ab95bf5a4ae08ebfe370c3afa4&units=metric`;
@@ -21,20 +19,16 @@ function App() {
       .then((res) => {
         if (res.statusText === "OK") {
           console.log(res.data);
-		  setDataError(false);
+          setDataError(false);
           setWeatherData({ ...res.data });
           setImgURL(
             `https://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`
           );
-        } else {
-        //   setWeatherData((prevData) => prevData);
-		setDataError(true);
-	}
-})
-.catch(() => {
-	console.log("Error");
-	setDataError(true);
-        // setWeatherData((prevData) => prevData);
+        }
+      })
+      .catch(() => {
+        console.log("Error");
+        setDataError(true);
       })
       .finally(() => {
         setLoading(false);
@@ -49,50 +43,73 @@ function App() {
     setActive(true);
   }
   return (
-    <div className={`flex flex-col items-center gap-3 w-screen h-screen bg-green-50`}>
-	<img className="w-screen h-screen absolute blur-lg opacity-80" src={randomSky} alt="" />
-	<div  className="text-3xl font-bold text-center text-gray-600 bg-opacity-60 bg-white p-6 rounded-lg drop-shadow-md z-1 w-2/5 mt-10">
-      <h1>Weather Forecast</h1>
-	</div>
+    <div
+      className={`flex flex-col items-center gap-3 w-screen h-screen bg-blue-200`}
+    >
+      <div className="text-3xl font-bold text-center text-gray-600 bg-opacity-60 bg-white p-6 rounded-lg drop-shadow-md z-1 w-2/5 mt-10">
+        <h1>Weather Forecast</h1>
+      </div>
 
-	  <div className=" text-lg rounded-xl drop-shadow-lg bg-opacity-60 bg-white p-6 text-gray-600 w-2/5">
-		<div className="flex flex-row justify-center items-center gap-2 mb-1 drop-shadow-lg">
-			<label htmlFor="city__field">Place Name: </label>
-			<input
-				id="city__field"
-				className="rounded-md bg-gray-100 p-2"
-				type="text"
-				value={cityName}
-				onChange={(e) => handleCityChange(e)}
-			/>
-			<button className="rounded-md bg-gray-500 p-2 text-white drop-shadow-lg" onClick={handleCitySet}>
-				OK
-			</button>
-		</div>
-		{loading ? (
-			"Loading..."
-		) : weatherData ? (
-			<div className="flex flex-col items-center gap-2">
-			<div className="flex flex-col align-middle justify-center gap-1">
-				<img className="drop-shadow-lg" src={imgURL} alt="Weather Image" />
-				<p className="text-gray-600 text-center drop-shadow-lg">{weatherData.weather[0].main} Sky</p>
-			</div>
-			<div className="w-3/5 m-auto drop-shadow-lg">
-				<p className="flex justify-between"><b>Temperature: </b>{weatherData.main.temp} °C</p>
-				<p className="flex justify-between"><b>Wind Speed: </b>{weatherData.wind.speed} km/h</p>
-				<p className="flex justify-between"><b>Humidity: </b>{weatherData.main.humidity} %</p>
-			</div>
-			<h2 className="text-3xl font-bold text-center drop-shadow-lg">{weatherData.name}</h2>
-			</div>
-		) : (
-			"No Data"
-		)}
-	  </div>
-	  {dataError ? 
-		<div className="error__msg text-lg text-center rounded-lg drop-shadow-lg bg-opacity-60 bg-white p-3 text-red-600 w-2/5">
-			No Data Found
-			</div>
-	  : ""}
+      <div className=" text-lg rounded-xl drop-shadow-lg bg-opacity-60 bg-white p-6 text-gray-600 w-2/5">
+        <div className="flex flex-row justify-center items-center gap-2 mb-1 drop-shadow-lg">
+          <label htmlFor="city__field">Place Name: </label>
+          <input
+            id="city__field"
+            className="rounded-md bg-gray-100 p-2"
+            type="text"
+            value={cityName}
+            onChange={(e) => handleCityChange(e)}
+          />
+          <button
+            className="rounded-md bg-gray-500 p-2 text-white drop-shadow-lg"
+            onClick={handleCitySet}
+          >
+            OK
+          </button>
+        </div>
+        {loading ? (
+          "Loading..."
+        ) : weatherData ? (
+          <div className="flex flex-col items-center gap-2">
+            <div className="flex flex-col align-middle justify-center gap-1">
+              <img
+                className="drop-shadow-lg"
+                src={imgURL}
+                alt="Weather Image"
+              />
+              <p className="text-gray-600 text-center drop-shadow-lg">
+                {weatherData.weather[0].main} Sky
+              </p>
+            </div>
+            <div className="w-3/5 m-auto drop-shadow-lg">
+              <p className="flex justify-between">
+                <b>Temperature: </b>
+                {weatherData.main.temp} °C
+              </p>
+              <p className="flex justify-between">
+                <b>Wind Speed: </b>
+                {weatherData.wind.speed} km/h
+              </p>
+              <p className="flex justify-between">
+                <b>Humidity: </b>
+                {weatherData.main.humidity} %
+              </p>
+            </div>
+            <h2 className="text-3xl font-bold text-center drop-shadow-lg">
+              {weatherData.name}
+            </h2>
+          </div>
+        ) : (
+          "No Data"
+        )}
+      </div>
+      {dataError ? (
+        <div className="error__msg text-lg text-center rounded-lg drop-shadow-lg bg-opacity-60 bg-white p-3 text-red-600 w-2/5">
+          No Data Found
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
